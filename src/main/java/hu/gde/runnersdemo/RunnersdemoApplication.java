03438da16449f257a115f2a752aab4990959a48a
Aron Boga <aron.boga@epsglobal.eu>
2023-05-03 23:52:05 +0200

    Inital commit

---
+++ src/main/java/hu/gde/runnersdemo/RunnersdemoApplication.java
@@ -0,0 +1,13 @@
+package hu.gde.runnersdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/gde/runnersdemo/RunnersdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gde.runnersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnersdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
