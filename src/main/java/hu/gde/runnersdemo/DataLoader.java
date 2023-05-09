b1c03553cd44aa0f09131bd4cc2a54d6e0d7d7f8
Aron Boga <aron.boga@epsglobal.eu>
2023-05-09 12:04:46 +0200

    Nem hasznalt importok kivetele

--- src/main/java/hu/gde/runnersdemo/DataLoader.java
+++ src/main/java/hu/gde/runnersdemo/DataLoader.java
@@ -3,8 +3,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class DataLoader implements CommandLineRunner {
--- src/main/java/hu/gde/runnersdemo/RunnerRestController.java
+++ src/main/java/hu/gde/runnersdemo/RunnerRestController.java
@@ -2,7 +2,6 @@ package hu.gde.runnersdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
